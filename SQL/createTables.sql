4f39f0466e82f0f1fb7bb59d1f5dbaeeadb73a36
kldxbjz <kldxbjz@github.com>
2022-11-13 17:31:07 -0500

    Update on data and populate python

--- SQL/createTables.sql
+++ SQL/createTables.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `User` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `UserProfile` (
-  `email` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL REFERENCES `User` (`email`),
   `user_name` varchar(45) NOT NULL,
   `phone_number` decimal(10,0) NOT NULL,
   PRIMARY KEY (`email`),
@@ -17,17 +17,17 @@ CREATE TABLE IF NOT EXISTS `UserProfile` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `DriverProfile` (
-  `email` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL REFERENCES `User` (`email`),
   `user_name` varchar(45) NOT NULL,
   `phone_number` decimal(10,0) NOT NULL,
-  `rating` decimal(1,1) NOT NULL DEFAULT '0.0',
+  `rating` decimal(2,1) NOT NULL DEFAULT '0.0',
   PRIMARY KEY (`email`),
   UNIQUE KEY `user_name_UNIQUE` (`user_name`),
   CONSTRAINT `driverprofile_chk_1` CHECK (((`rating` <= 5) and (`rating` >= 0)))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `Posting` (
-  `post_id` varchar(36) NOT NULL,
+  `post_id` varchar(36) NOT NULL REFERENCES `ProvideCarpool` (`post_id`),
   `pickup_location` varchar(45) NOT NULL,
   `dropoff_location` varchar(45) NOT NULL,
   `pickup_time` datetime NOT NULL,
@@ -38,41 +38,39 @@ CREATE TABLE IF NOT EXISTS `Posting` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `ProvideCarpool` (
-  `email` int NOT NULL,
+  `email` varchar(40) NOT NULL REFERENCES `DriverProfile` (`email`),
   `post_id` varchar(45) NOT NULL,
-  PRIMARY KEY (`post_id`),
-  CONSTRAINT `post_id_fkey` FOREIGN KEY (`post_id`) REFERENCES `Posting` (`post_id`)
+  PRIMARY KEY (`post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `Reservation` (
   `reservation_id` varchar(36) NOT NULL,
-  `posting_id` varchar(36) NOT NULL,
-  `user_email` varchar(45) NOT NULL,
+  `posting_id` varchar(36) NOT NULL REFERENCES `Posting` (`post_id`),
+  `user_email` varchar(45) NOT NULL REFERENCES `UserProfile` (`email`) ON DELETE CASCADE,
   PRIMARY KEY (`reservation_id`),
   KEY `post_id_idx` (`posting_id`),
-  KEY `email_idx` (`user_email`),
-  CONSTRAINT `email` FOREIGN KEY (`user_email`) REFERENCES `UserProfile` (`email`) ON DELETE CASCADE,
-  CONSTRAINT `post_id` FOREIGN KEY (`posting_id`) REFERENCES `Posting` (`post_id`)
+  KEY `email_idx` (`user_email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `Review` (
-  `reservation_id` varchar(36) NOT NULL,
-  `driver_email` varchar(45) NOT NULL,
+  `reservation_id` varchar(36) NOT NULL REFERENCES `Reservation` (`reservation_id`),
+  `driver_email` varchar(45) NOT NULL REFERENCES `DriverProfile` (`email`),
   `rating` decimal(1,0) NOT NULL DEFAULT '5',
-  `comment` varchar(120) DEFAULT NULL,
+  `comment` varchar(150) DEFAULT NULL,
+  `time` datetime NOT NULL,
   PRIMARY KEY (`reservation_id`),
   KEY `email_idx` (`driver_email`),
   KEY `email_index` (`driver_email`),
-  CONSTRAINT `driver_email_fkey` FOREIGN KEY (`driver_email`) REFERENCES `DriverProfile` (`email`),
-  CONSTRAINT `reservarion_id_fkey` FOREIGN KEY (`reservation_id`) REFERENCES `Reservation` (`reservation_id`)
+  CONSTRAINT `review_chk_1` CHECK (((`rating` <= 5) and (`rating` >= 0)))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE DEFINER = CURRENT_USER TRIGGER `test`.`Review_AFTER_INSERT` AFTER INSERT ON `Review` FOR EACH ROW
+CREATE DEFINER = CURRENT_USER TRIGGER `test`.`Review_AFTER_INSERT` AFTER INSERT ON `Review` 
+FOR EACH ROW
 	UPDATE DriverProfile
     SET rating = (
 		SELECT AVG(rating)
         FROM Review
+        WHERE driver_email = DriverProfile.email
         GROUP BY driver_email
-        HAVING driver_email = email
     )
-
+    WHERE NEW.driver_email = email
