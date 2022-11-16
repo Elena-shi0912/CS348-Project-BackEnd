04237cc97441fdcc8cfc3325950db2799017c6a1
Jane Chen <janechen@Macintosh.ht.home>
2022-11-15 20:59:22 -0500

    Update to first 2 feature

--- Sample_Data_M2/populate_sample_data.sql
+++ Sample_Data_M2/populate_sample_data.sql
@@ -1,27 +1,27 @@
-LOAD DATA LOCAL INFILE '/Users/syy/Desktop/CS348_Project/Sample_Data_M2/User.csv'
+LOAD DATA INFILE '/Users/janechen/Desktop/FALL2022/CS348/Project/Milestone 2/CS348-Project-BackEnd/CS348-Project-BackEnd/Sample_Data_M2/User.csv'
 INTO TABLE carpool.User FIELDS TERMINATED BY ','
 ENCLOSED BY '"' LINES TERMINATED BY '\n';
 
-LOAD DATA LOCAL INFILE '/Users/syy/Desktop/CS348_Project/Sample_Data_M2/UserProfile.csv'
+LOAD DATA INFILE '/Users/janechen/Desktop/FALL2022/CS348/Project/Milestone 2/CS348-Project-BackEnd/CS348-Project-BackEnd/Sample_Data_M2/UserProfile.csv'
 INTO TABLE carpool.UserProfile FIELDS TERMINATED BY ','
 ENCLOSED BY '"' LINES TERMINATED BY '\n';
 
-LOAD DATA LOCAL INFILE '/Users/syy/Desktop/CS348_Project/Sample_Data_M2/DriverProfile.csv'
+LOAD DATA INFILE '/Users/janechen/Desktop/FALL2022/CS348/Project/Milestone 2/CS348-Project-BackEnd/CS348-Project-BackEnd/Sample_Data_M2/DriverProfile.csv'
 INTO TABLE carpool.DriverProfile FIELDS TERMINATED BY ','
 ENCLOSED BY '"' LINES TERMINATED BY '\n';
 
-LOAD DATA LOCAL INFILE '/Users/syy/Desktop/CS348_Project/Sample_Data_M2/ProvideCarpool.csv'
+LOAD DATA INFILE '/Users/janechen/Desktop/FALL2022/CS348/Project/Milestone 2/CS348-Project-BackEnd/CS348-Project-BackEnd/Sample_Data_M2/ProvideCarpool.csv'
 INTO TABLE carpool.ProvideCarpool FIELDS TERMINATED BY ','
 ENCLOSED BY '"' LINES TERMINATED BY '\n';
 
-LOAD DATA LOCAL INFILE '/Users/syy/Desktop/CS348_Project/Sample_Data_M2/Posting.csv'
+LOAD DATA INFILE '/Users/janechen/Desktop/FALL2022/CS348/Project/Milestone 2/CS348-Project-BackEnd/CS348-Project-BackEnd/Sample_Data_M2/Posting.csv'
 INTO TABLE carpool.Posting FIELDS TERMINATED BY ','
 ENCLOSED BY '"' LINES TERMINATED BY '\n';
 
-LOAD DATA LOCAL INFILE '/Users/syy/Desktop/CS348_Project/Sample_Data_M2/Reservation.csv'
+LOAD DATA INFILE '/Users/janechen/Desktop/FALL2022/CS348/Project/Milestone 2/CS348-Project-BackEnd/CS348-Project-BackEnd/Sample_Data_M2/Reservation.csv'
 INTO TABLE carpool.Reservation FIELDS TERMINATED BY ','
 ENCLOSED BY '"' LINES TERMINATED BY '\n';
 
-LOAD DATA LOCAL INFILE '/Users/syy/Desktop/CS348_Project/Sample_Data_M2/Review.csv'
+LOAD DATA INFILE '/Users/janechen/Desktop/FALL2022/CS348/Project/Milestone 2/CS348-Project-BackEnd/CS348-Project-BackEnd/Sample_Data_M2/Review.csv'
 INTO TABLE carpool.Review FIELDS TERMINATED BY ','
 ENCLOSED BY '"' LINES TERMINATED BY '\n';
--- index.js
+++ index.js
@@ -1,77 +1,99 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const bodyparser = require('body-parser');
 const app = express();
 const mysql = require('mysql');
+// const { 
+//   v1: uuidv1,
+//   v4: uuidv4,
+// } = require('uuid');
+const { emit } = require('nodemon');
+const cors = require('cors');
+let currentUser = 1 ; // true if current user is a driver
+let currentUserEmail = "";
+
 
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '20020912@Syy',
-    database: 'CS348_Project',
-})
-
-db.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected to Database!");
-    var sqlCreate = 
-        "CREATE TABLE UserInfoTemp (UserID VARCHAR(36) NOT NULL DEFAULT(uuid()), Password VARCHAR(15) NOT NULL, UserName VARCHAR(15) NOT NULL, Phone DECIMAL(10,0) NOT NULL, Email VARCHAR(25) NOT NULL, PRIMARY KEY (UserID));";
-    db.query(sqlCreate, function(err, result) {
-        if (err) throw err;
-        console.log("User Table Created!");
-    });
-    sqlCreate =
-        "CREATE TABLE DriverInfoTemp (DriverID VARCHAR(36) NOT NULL DEFAULT(uuid()), DriverName VARCHAR(15) NOT NULL, PassWord VARCHAR(15) NOT NULL, License_Number CHAR(15) NOT NULL, Year_Of_Driving DECIMAL(2,0) NOT NULL, Phone DECIMAL(10) NOT NULL, Email VARCHAR(25) NOT NULL, PRIMARY KEY (DriverID));";
-    db.query(sqlCreate, function(err, result) {
-        if (err) throw err;
-        console.log("Driver Table Created!");
-    });
-
-    sqlCreate =
-        "CREATE TABLE CarInfoTemp (CarID VARCHAR(36) NOT NULL DEFAULT(uuid()), Driver_ID VARCHAR(36) NOT NULL, Car_Maker VARCHAR(20) NOT NULL, Car_Color VARCHAR(15) NOT NULL, Car_Model VARCHAR(25) NOT NULL, Year_Of_Car DECIMAL(2,0) NOT NULL, Car_Plate_Number VARCHAR(8) NOT NULL, PRIMARY KEY (CarID), INDEX DriverID_idx (Driver_ID ASC) VISIBLE, CONSTRAINT DriverID FOREIGN KEY (Driver_ID) REFERENCES DriverInfo (DriverID) ON DELETE CASCADE ON UPDATE NO ACTION);";
-    db.query(sqlCreate, function(err, result) {
-        if (err) throw err;
-        console.log("Car Table Created!");
-    });
-
-    sqlCreate = 
-        "CREATE TABLE PostInfo (PostID VARCHAR(36) NOT NULL DEFAULT(uuid()), CarID VARCHAR(36) NOT NULL, Pickup_Location VARCHAR(50) NOT NULL, Dropoff_Location VARCHAR(50) NOT NULL, Pickup_Date DATE NOT NULL, Pickup_Time TIME NOT NULL, Seats_Available DECIMAL(1,0) NOT NULL, Price_Per_Seat DECIMAL(4,0) NOT NULL, Additionl_Info VARCHAR(120) NULL, PRIMARY KEY (PostID), INDEX CarID_idx (CarID ASC) VISIBLE, CONSTRAINT CarID FOREIGN KEY (CarID) REFERENCES CarInfo (CarID) ON DELETE CASCADE ON UPDATE NO ACTION);";
-    db.query(sqlCreate, function(err, result) {
-        if (err) throw err;
-        console.log("Posting Table Created!");
-    });
-
-    sqlCreate =
-        "CREATE TABLE Review (PostID VARCHAR(36) NOT NULL, UserID VARCHAR(36) NOT NULL, Rating DECIMAL(1,0) NULL, Comment VARCHAR(150) NULL, CommentTime DATETIME NULL, PRIMARY KEY (PostID, UserID), INDEX UserID_idx (UserID ASC) VISIBLE, CONSTRAINT PostID FOREIGN KEY (PostID) REFERENCES PostInfo (PostID) ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT UserID FOREIGN KEY (UserID) REFERENCES UserInfo (UserID) ON DELETE CASCADE ON UPDATE NO ACTION);";
-    db.query(sqlCreate, function(err, result) {
-        if (err) throw err;
-        console.log("Review Table Created!");
-    });
-    
+    password: 'idiot$123',
+    database: 'carpool',
 });
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyparser.urlencoded({extended: true}));
 
-app.post("/api/init", (req, res) => {
-    res.send("populate database with sample data");
-    const sqlLoad = 
-        "LOAD DATA LOCAL INFILE '~/Users/syy/cs348_project/server/UserInfo.txt' INTO TABLE UserInfo LINES TERMINATED BY '\r';";
-})
+db.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
+app.post('/api/g', (req, res)=> {
+  const sqlSelect = "SELECT * FROM user";
+  db.query(sqlSelect, (err, result) => {
+    //console.log(result);
+    res.send(result);
+  })
+});
+
+
+// create account, insert to user table
 app.post("/api/insert", (req, res) => {
-    res.send("hello to backend world");
-    const userName = req.body.userName;
-    const userEmail = req.body.userEmail;
+  const email = req.body.userEmail;
+  const password = req.body.password;
+  const isDriver = req.body.isDriver;
+  const sqlinsert = "INSERT INTO user values(?,?,?);";
+  db.query(sqlinsert,[email, password, isDriver], (err, result)=> {
+    res.send("success insert!");
+   });
+});
 
-    const sqlInsert = 
-        "INSERT INTO UserInfo (userName, userEmail) VALUES (?,?);";
-    db.query(sqlInsert, [userName, userEmail], (err, result) => {
-        console.log(err);
-    });
+// log in, check if password correct
+app.post("/api/check", (req, res) => {
+  //const email = req.body.userEmail;
+  //const password = req.body.password;
+  const sqlCheckDriver = "SELECT isDriver FROM user WHERE email = (?);";
+  const userEmail = req.body.userEmail;
+  const password = req.body.password;
+  currentUserEmail = req.body.userEmail;
+  db.query(sqlCheckDriver, [userEmail], (err, result) => {
+    currentUser = result[0].isDriver;
+    console.log(currentUser);
+  });
+  const sqlCheck = "SELECT COUNT(*) AS userCount FROM user WHERE email = (?) AND password = (?);";
+  db.query(sqlCheck, [userEmail, password], (err, result) => {
+    if (result[0].userCount == 1) {
+      res.send(true);
+    } else {
+      res.send(false);
+    }
+  });
+});
+
+// display posts
+app.post("api/dbinfo", (req, res) => {
+  const info = req.body.info;
+  console.log("Here");
+  // send posting information
+  if (info == "I need post") {
+    // check if current user is a driver
+    console.log("Here");
+    if (currentUser) {
+      console.log("Driver");
+      const sqlGetPost = "SELECT * FROM posting NATURAL JOIN ProvideCarpool WHERE email = ?;";
+      db.query(sqlGetPost, [currentUserEmail], (err, result) => {
+        res.send(result);
+      });
+    } else {
+      console.log("Not Driver");
+      const sqlGetPost = "SELECT * FROM posting;"
+      db.query(sqlGetPost, (err, result) => {
+        res.send(result);
+      });
+    }
+  }
 });
 
 app.listen(3001, () => {
-    console.log("running on port 3001");
+  console.log("running on port 3001");
 });
