e92334932b7b398b7e91b5d23fa5df9c5f1fedad
Yunyi Shi <shiyunyi2002@outlook.com>
2022-11-30 12:31:24 -0500

    Update features

--- index.js
+++ index.js
@@ -67,7 +67,6 @@ app.post("/api/check", (req, res) => {
   const password = req.body.password;
   currentUserEmail = req.body.userEmail;
   db.query(sqlCheckDriver, [userEmail], (err, result) => {
-    console.log(result);
     currentUser = result[0].isDriver;
     console.log(currentUser);
   });
@@ -89,10 +88,14 @@ app.post("/api/check", (req, res) => {
 app.post("/api/dbinfo", (req, res) => {
     // if log in as user, only postings that are not reserved by the user should be displayed
     // if log in as driver, only his/her own postings should be displayed
+    // NOTE: only ongoing posting will be displayed
+    console.log("=====HERE=====")
+    const datetime = req.body.datetime;
+    console.log(datetime)
     if (currentUser) {
-      console.log("Driver");
-      const sqlGetPost = "SELECT * FROM posting NATURAL JOIN ProvideCarpool WHERE email = ?;";
-      db.query(sqlGetPost, [currentUserEmail], (err, result) => {
+      console.log("Display Driver ongoing posting");
+      const sqlGetPost = "SELECT * FROM posting NATURAL JOIN ProvideCarpool WHERE pickup_time > ? AND email = ?;";
+      db.query(sqlGetPost, [datetime, currentUserEmail], (err, result) => {
         res.send(result);
       });
     } else {
@@ -104,16 +107,16 @@ app.post("/api/dbinfo", (req, res) => {
       // if user do not apply any selection or ordering requirement
       if (FromLoc == "" && ToLoc == "" && Sort == "" && ordering == "") {
         console.log("NO FILTER");
-        const sqlGetPost = "SELECT * FROM posting WHERE post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?);"
-        db.query(sqlGetPost, [currentUserEmail + '%'], (err, result) => {
+        const sqlGetPost = "SELECT * FROM posting WHERE pickup_time > ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?);"
+        db.query(sqlGetPost, [datetime, currentUserEmail + '%'], (err, result) => {
           console.log(err);
           res.send(result);
         });
       } else if (FromLoc != "" && ToLoc != "" && Sort == "" && ordering == "") {
         // User only apply selection
         console.log("FILTER");
-        const sqlGetPost = "SELECT * FROM posting WHERE pickup_location = ? AND dropoff_location = ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?);"
-        db.query(sqlGetPost, [FromLoc, ToLoc, currentUserEmail + '%'], (err, result) => {
+        const sqlGetPost = "SELECT * FROM posting WHERE pickup_time > ? AND pickup_location = ? AND dropoff_location = ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?);"
+        db.query(sqlGetPost, [datetime, FromLoc, ToLoc, currentUserEmail + '%'], (err, result) => {
           console.log(err);
           res.send(result);
         });
@@ -123,33 +126,33 @@ app.post("/api/dbinfo", (req, res) => {
         if (Sort == "Price") {
           var sqlGetPost;
           if (ordering == "Ascending") {
-            sqlGetPost = "SELECT * FROM posting WHERE post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY price_per_seat";
+            sqlGetPost = "SELECT * FROM posting WHERE pickup_time > ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY price_per_seat";
           } else {
-            sqlGetPost = "SELECT * FROM posting WHERE post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY price_per_seat DESC";
+            sqlGetPost = "SELECT * FROM posting WHERE pickup_time > ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY price_per_seat DESC";
           }
-          db.query(sqlGetPost, [currentUserEmail + '%'], (err, result) => {
+          db.query(sqlGetPost, [datetime, currentUserEmail + '%'], (err, result) => {
             console.log(err);
             res.send(result);
           })
         } else if (Sort == "Seats Available") {
           var sqlGetPost;
           if (ordering == "Ascending") {
-            sqlGetPost = "SELECT * FROM posting WHERE post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY available_seats";
+            sqlGetPost = "SELECT * FROM posting WHERE pickup_time > ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY available_seats";
           } else {
-            sqlGetPost = "SELECT * FROM posting WHERE post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY available_seats DESC";
+            sqlGetPost = "SELECT * FROM posting WHERE pickup_time > ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY available_seats DESC";
           }
-          db.query(sqlGetPost, [currentUserEmail + '%'], (err, result) => {
+          db.query(sqlGetPost, [datetime, currentUserEmail + '%'], (err, result) => {
             console.log(err);
             res.send(result);
           })
         } else {
           var sqlGetPost;
           if (ordering = "Ascending") {
-            sqlGetPost = "SELECT * FROM posting WHERE post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY pickup_time";
+            sqlGetPost = "SELECT * FROM posting WHERE pickup_time > ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY pickup_time";
           } else {
-            sqlGetPost = "SELECT * FROM posting WHERE post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY pickup_time DESC";
+            sqlGetPost = "SELECT * FROM posting WHERE pickup_time > ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY pickup_time DESC";
           }
-          db.query(sqlGetPost, [currentUserEmail + '%'], (err, result) => {
+          db.query(sqlGetPost, [datetime, currentUserEmail + '%'], (err, result) => {
             console.log(err);
             res.send(result);
           })
@@ -160,33 +163,33 @@ app.post("/api/dbinfo", (req, res) => {
         if (Sort == "Price") {
           var sqlGetPost;
           if (ordering = "Ascending") {
-            sqlGetPost = "SELECT * FROM posting WHERE pickup_location = ? AND dropoff_location = ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY price_per_seat";
+            sqlGetPost = "SELECT * FROM posting WHERE pickup_time > ? AND pickup_location = ? AND dropoff_location = ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY price_per_seat";
           } else {
-            sqlGetPost = "SELECT * FROM posting WHERE pickup_location = ? AND dropoff_location = ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY price_per_seat DESC";
+            sqlGetPost = "SELECT * FROM posting WHERE pickup_time > ? AND pickup_location = ? AND dropoff_location = ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY price_per_seat DESC";
           }
-          db.query(sqlGetPost, [FromLoc, ToLoc, currentUserEmail + '%'], (err, result) => {
+          db.query(sqlGetPost, [datetime, FromLoc, ToLoc, currentUserEmail + '%'], (err, result) => {
             console.log(err);
             res.send(result);
           })
         } else if (Sort == "Seats Available") {
           var sqlGetPost;
           if (ordering == "Ascending") {
-            sqlGetPost = "SELECT * FROM posting WHERE pickup_location = ? AND dropoff_location = ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY available_seats";
+            sqlGetPost = "SELECT * FROM posting WHERE pickup_time > ? AND pickup_location = ? AND dropoff_location = ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY available_seats";
           } else {
-            sqlGetPost = "SELECT * FROM posting WHERE pickup_location = ? AND dropoff_location = ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY available_seats DESC";
+            sqlGetPost = "SELECT * FROM posting WHERE pickup_time > ? AND pickup_location = ? AND dropoff_location = ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY available_seats DESC";
           }
-          db.query(sqlGetPost, [FromLoc, ToLoc, currentUserEmail + '%'], (err, result) => {
+          db.query(sqlGetPost, [datetime, FromLoc, ToLoc, currentUserEmail + '%'], (err, result) => {
             console.log(err);
             res.send(result);
           })
         } else {
           var sqlGetPost;
           if (ordering == "Ascending") {
-            sqlGetPost = "SELECT * FROM posting WHERE pickup_location = ? AND dropoff_location = ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY pickup_time";
+            sqlGetPost = "SELECT * FROM posting WHERE pickup_time > ? AND pickup_location = ? AND dropoff_location = ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY pickup_time";
           } else {
-            sqlGetPost = "SELECT * FROM posting WHERE pickup_location = ? AND dropoff_location = ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY pickup_time DESC";
+            sqlGetPost = "SELECT * FROM posting WHERE pickup_time > ? AND pickup_location = ? AND dropoff_location = ? AND post_id NOT IN (SELECT posting_id FROM Reservation WHERE user_email LIKE ?) ORDER BY pickup_time DESC";
           }
-          db.query(sqlGetPost, [FromLoc, ToLoc, currentUserEmail + '%'], (err, result) => {
+          db.query(sqlGetPost, [datetime, FromLoc, ToLoc, currentUserEmail + '%'], (err, result) => {
             console.log(err);
             res.send(result);
           })
@@ -196,45 +199,48 @@ app.post("/api/dbinfo", (req, res) => {
 });
 
 // display user information
-app.get("/api/accountinfo", (req, res) => {
-    //const email = currentUserEmail;
-    const email = "Eason@gmail.com"; //
+app.post("/api/accountinfo", (req, res) => {
+    // //const email = currentUserEmail;
+    // const email = "Eason@gmail.com"; //
+    console.log("want to access user info")
     if (currentUser) {
       console.log("Driver");
-      const sqlGetPost = "SELECT * FROM user NATURAL JOIN driverprofile WHERE email = ?;";
-      db.query(sqlGetPost, [email], (err, result) => {
+      const sqlGetPost = "SELECT * FROM driverprofile WHERE email = ?;";
+      db.query(sqlGetPost, [currentUserEmail], (err, result) => {
         res.send(result);
+        console.log(result);
       });
     } else {
       console.log("Not Driver");
-      const sqlGetPost = "SELECT * FROM user NATURAL JOIN userprofile WHERE email = ?;";
-      db.query(sqlGetPost, [email], (err, result) => {
+      const sqlGetPost = "SELECT * FROM userprofile WHERE email = ?;";
+      db.query(sqlGetPost, [currentUserEmail], (err, result) => {
         res.send(result);
       });
     }
   //}
 });
 
-// Update seats available in posting
-app.post("/api/updateseats", (req, res) => {
-  const post_id = req.body.post_id;
-  const available_seats = req.body.available_seats;
-  const sqlupdate = "UPDATE posting SET available_seats = ? WHERE post_id = ?;";
-  db.query(sqlupdate,[available_seats, post_id], (err, result)=> {
-    res.send("successful update!");
-   });
-});
-
-// display user reservations
+// display user's ongoing reservations
 app.post("/api/reserveInfo", (req, res)=> {
-  const sqlreserve = "SELECT * FROM posting WHERE post_id IN (SELECT posting_id FROM reservation WHERE user_email LIKE ?);"
-  db.query(sqlreserve, [currentUserEmail + '%'], (err, result) => {
+  const time = req.body.datetime; // current time get from frontend
+  const sqlreserve = "SELECT * FROM posting WHERE pickup_time > ? AND post_id IN (SELECT posting_id FROM reservation WHERE user_email LIKE ?);"
+  db.query(sqlreserve, [time, currentUserEmail + '%'], (err, result) => {
     res.send(result);
     console.log(currentUserEmail);
     console.log("Display user reservation");
   })
 })
 
+// display user's past reservations that have not been reviewed
+app.post("/api/reviewInfo", (req, res) => {
+  const time = req.body.datetime;
+  const sqlpast = "SELECT * FROM posting WHERE pickup_time < ? AND post_id IN (SELECT posting_id FROM reservation WHERE user_email LIKE ? AND reservation_id NOT IN (SELECT reservation_id from Review WHERE Reservation.reservation_id = Review.reservation_id));"
+  db.query(sqlpast,[time, currentUserEmail + '%'], (err, result) => {
+    console.log("showing users past unreviewed reservations");
+    console.log(err);
+    res.send(result);
+  })
+})
 
 // make reservations
 app.post("/api/reserve", (req, res) => {
@@ -250,7 +256,7 @@ app.post("/api/reserve", (req, res) => {
   }
 });
 
-// make reservations
+// cancel reservations
 app.post("/api/cancel", (req, res) => {
   console.log(req.body.post_id);
   if (currentUser == false){ // Current user can make reservation if one is not a driver
@@ -263,6 +269,59 @@ app.post("/api/cancel", (req, res) => {
   }
 });
 
+// Driver create posting
+app.post("/api/addPosting", (req,res) => {
+  console.log(currentUserEmail+ " wants to create new post");
+  const new_post_id = uuid.v4();
+  const fromLoc = req.body.from;
+  const toLoc = req.body.to;
+  const seats = req.body.seats;
+  const price = req.body.price;
+  const info = req.body.info;
+  const datetime = req.body.datetime;
+  console.log(datetime);
+  // insert into provideCarpool first
+  var sqlCreatePost = "INSERT INTO ProvideCarpool value(?,?);"
+  db.query(sqlCreatePost,[currentUserEmail, new_post_id], (err,result) => {
+    console.log(err);
+    console.log("create provideCarpool record successfully");
+  })
+  sqlCreatePost = "INSERT INTO Posting value(?,?,?,?,?,?,?);"
+  db.query(sqlCreatePost,[new_post_id,fromLoc,toLoc,datetime,seats,price,info],(err,result) => {
+    console.log(err)
+    console.log("successfully create posting")
+    res.send("Your new post is created!")
+  })
+})
+
+// User make review
+app.post("/api/addReview", (req, res) => {
+  console.log(currentUserEmail + " is going to make review");
+  const post_id = req.body.post_id;
+  const rating = req.body.rating;
+  const comment = req.body.comment;
+  const time = req.body.datetime;
+  var reservation_id;
+  var driverEmail = "";
+  const sqlFindReservation = "SELECT reservation_id FROM Reservation WHERE posting_id = ?;"
+  db.query(sqlFindReservation, [post_id], (err, result) => {
+    console.log("Find out corresponding reservation id")
+    console.log(err)
+    reservation_id = result[0].reservation_id;
+    const sqlFindDriver = "SELECT email FROM ProvideCarpool WHERE post_id = ?;"
+    db.query(sqlFindDriver,[post_id],(err, result) => {
+      console.log("Find out corresponding driver")
+      console.log(err)
+      driverEmail = result[0].email;
+      const sqlAddReview = "INSERT INTO Review value(?,?,?,?,?);"
+      db.query(sqlAddReview,[reservation_id,driverEmail,rating,comment,time], (err, result) => {
+        console.log(err)
+        console.log(currentUserEmail + " add review to reservation with id " + reservation_id + " provided by " + driverEmail);
+        res.send("Your review is added successfully");
+      })
+    })
+  })
+})
 
 app.listen(3001, () => {
   console.log("running on port 3001");
